808ab69e7735f39ca00c0bc8b5e08ab0dc430b77
Acu <wongethan120@gmail.com>
2026-09-06 03:21:52 +0000

    asdf

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,26 @@
+let box;
+let floor;
 
+function setup() {
+    new Canvas(600,400);
+
+    world.gravity.y = 10;
+    
+    box = new Sprite();
+    box.x = width/2;
+    box.y = height/2;
+    box.height = 30;
+    box.width = 30;
+    
+
+    floor = new Sprite();
+    floor.x = width/2;
+    floor.y = height;
+    floor.height = 20;
+    floor.width = 2400;
+    floor.collider = "static";
+}
+
+function draw() {
+    clear();
+}
